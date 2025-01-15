8a1fe00bf4f903bc5f754f950d51cb3128c277f3
waffleemilky <6sun6light6@gmail.com>
2025-01-15 22:25:31 +0300

    Homework5 Fix

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,35 @@ public class Main {
     public static void main(String[] args) {
 
         int clientOS = 1;
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для IOS по ссылке");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке");
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 
 
         int clientDeviceYear = 2015;
-        if (clientDeviceYear < 2015) {
-            switch (clientOS) {
-                case 0:
-                    System.out.println("Установите облегчённую версию приложения для IOS по ссылке");
-                    break;
-                case 1:
-                    System.out.println("Установите облегчённую версию приложения для Android по ссылке");
-            }
-        } else switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для IOS по ссылке");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке");
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегчённую версию приложения для IOS по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегчённую версию приложения для Android по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 
 
         int year = 2024;
-        if (year > 1584) {
-            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-                System.out.println(year + " год является високосным");
+        boolean year2 = ((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0));
+        if (year > 1584 && year2){
+            System.out.println(year + " год является високосным");
             } else {
-                System.out.println(year + " год не является високосным");
+            System.out.println(year + " год не является високосным");
             }
-        } else {
+        if (year < 1584) {
             System.out.println("Год не может быть меньше 1585");
         }
 
